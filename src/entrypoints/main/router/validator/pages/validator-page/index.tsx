86effdee0074be0cc684fc381fb22048017a9c96
Nils Ove Tendenes <nils.ove@tendenes.com>
2025-03-26 14:04:30 +0100

    feat: replace validator page content with warning

--- src/entrypoints/main/router/validator/pages/validator-page/index.tsx
+++ src/entrypoints/main/router/validator/pages/validator-page/index.tsx
@@ -1,6 +1,7 @@
-import React, { memo, FC, useEffect } from 'react';
+import React, { memo, FC } from 'react';
 import { compose } from 'redux';
 import { Severity } from '@fellesdatakatalog/alert';
+import Link from '@fellesdatakatalog/link';
 
 import withValidator, {
   Props as ValidatorProps
@@ -8,53 +9,20 @@ import withValidator, {
 
 import SC from './styled';
 
-import type { ValidationRequest } from '../../../../../../types';
-import { useQuery } from '../../../../../../utils/commons';
-
 interface Props extends ValidatorProps {}
 
-const ValidatorPage: FC<Props> = ({
-  validationReport,
-  validationError,
-  isValidating,
-  validatorActions: { validateDataGraphRequested: validateDataGraph }
-}) => {
-  const query = useQuery();
-  const dataGraph = decodeURIComponent(query.get('data') ?? '');
-  const shapesGraph = decodeURIComponent(query.get('shapes') ?? '');
-  const ontologyGraph = decodeURIComponent(query.get('ontology') ?? '');
-
-  const onValidate = (request: ValidationRequest) => validateDataGraph(request);
-
-  useEffect(() => {
-    if (query.has('data') && query.has('shapes')) {
-      onValidate({
-        dataGraph,
-        shapesGraph,
-        ontologyGraph,
-        config: { expand: true, includeExpandedTriples: false }
-      });
-    }
-  }, []);
-
-  return (
-    <SC.ValidatorPage>
-      <SC.Title>Valideringsverktøy</SC.Title>
-      <SC.ValidationForm
-        dataGraph={dataGraph}
-        shapesGraph={shapesGraph}
-        ontologyGraph={ontologyGraph}
-        expand
-        includeExpandedTriples={false}
-        isLoading={isValidating}
-        onValidate={onValidate}
-      />
-      {validationError && (
-        <SC.Alert severity={Severity.ERROR}>{validationError.message}</SC.Alert>
-      )}
-      {validationReport && <SC.ValidationReport report={validationReport} />}
-    </SC.ValidatorPage>
-  );
-};
+const ValidatorPage: FC<Props> = () => (
+  <SC.ValidatorPage>
+    <SC.Alert severity={Severity.WARNING}>
+      <span>
+        Validatoren er tatt ned på grunn av nødvendig vedlikehold. Mer
+        informasjon kommer.
+      </span>
+      <Link href='https://data.norge.no/nb/contact'>
+        Ta kontakt med oss hvis du har spørsmål rundt dette.
+      </Link>
+    </SC.Alert>
+  </SC.ValidatorPage>
+);
 
 export default compose<FC>(memo, withValidator)(ValidatorPage);
